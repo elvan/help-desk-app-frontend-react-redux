4840de092b10a1178af291c8a6cbf203ebdf30e5
Elvan Hidayat <elvanhd@gmail.com>
2023-03-02 05:39:34 +0700

    login and logout user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-export default function App() {
+function App() {
   return (
     <>
       <Router>
@@ -17,6 +19,9 @@ export default function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,48 @@
-import { FaSignInAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout, reset } from '../features/auth/authSlice';
 
-export default function Header() {
+function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/');
+  };
+
   return (
     <header className='header'>
       <div className='logo'>
         <Link to='/'>HelpDesk</Link>
       </div>
       <ul>
-        <>
+        {user ? (
           <li>
-            <Link to='/login'>
-              <FaSignInAlt /> Login
-            </Link>
+            <button className='btn' onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
           </li>
-          <li>
-            <Link to='/register'>
-              <FaUser /> Register
-            </Link>
-          </li>
-        </>
+        ) : (
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
 }
+
+export default Header;
